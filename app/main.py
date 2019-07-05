3686dcb2bd585b1e7017651517195710f5a766f5
jrpresta <jonrosspresta@gmail.com>
2019-07-05 13:39:19 -0700

    currently the team site (my own) imports the navbar in a reasonable way and can display my name and motto; embedding the YouTube video pushes the name upwards. Might disregard YouTube videos all together to display stats instead

--- app/main.py
+++ app/main.py
@@ -5,3 +5,23 @@ from flask import render_template
 @application.route('/')
 def index():
    return render_template('frontpage.html')
+
+
+# TODO: Maybe add the team name to pass as a parameter to return the
+@application.route('/teams/jon_ross')
+def jon_ross():
+    return render_template('teams/jon_ross.html')
+
+
+@application.route('/testing')
+def testing():
+    return render_template('base.html')
+
+
+@application.route('/testing2')
+def testing2():
+    return render_template('child_test.html')
+
+@application.route('/testing3')
+def testing3():
+    return render_template('team_parent.html')
